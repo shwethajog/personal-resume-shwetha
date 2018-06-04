c8c3170fc25f7519a7a763c73b93b5d1d158e59f
Shwetha Jog <shwethajog45@gmail.com>
2018-06-04 20:29:58 +0530

    Create default.js

---
+++ src/components/styles/default.js
@@ -0,0 +1,43 @@
+export const gridStyleLeft = {
+    margin: 0,
+    padding: 0
+};
+
+export const segmentStyleLeft = {
+    marginTop: 0,
+    marginLeft: 0,
+    marginBottom: 0,
+    marginRight: 0,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    border: 0
+};
+
+export const segmentStyleLeftFirst = {
+    marginTop: 0,
+    marginLeft: 0,
+    marginBottom: 0,
+    marginRight: 0,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    border: 0
+};
+
+export const segmentStyleRight = {
+    marginTop: 0,
+    marginLeft: 20,
+    marginBottom: 0,
+    marginRight: 0,
+    paddingLeft: 10,
+    paddingRight: 20,
+    paddingTop: 20,
+    paddingBottom: 10,
+    border: 0
+};
+
+
+export default {gridStyleLeft, segmentStyleLeft, segmentStyleLeftFirst, segmentStyleRight}
